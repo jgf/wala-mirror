7e239b4138d9eade559cae3f12011f41e3cbd922
Juergen Graf <juergen.graf@gmail.com>
2012-05-16 22:17:02 +0200

    refactored smali interface

--- edu.kit.wala.smali.test/src/edu/kit/wala/smali/test/basic/CallDexAnalysis.java
+++ edu.kit.wala.smali.test/src/edu/kit/wala/smali/test/basic/CallDexAnalysis.java
@@ -1,14 +1,14 @@
 package edu.kit.wala.smali.test.basic;
 
-import org.jf.dexlib.Interface.DexAnalysis;
+import org.jf.baksmali.Interface.BakSmaliAnalysis;
 import org.jf.dexlib.Interface.DexAnalysis.DexAnalysisException;
 import org.jf.dexlib.Interface.DexProgram;
 
 public class CallDexAnalysis {
 	public static void main(String[] args) throws DexAnalysisException {
-		final DexAnalysis.Config conf = new DexAnalysis.Config();
+		final BakSmaliAnalysis.Config conf = new BakSmaliAnalysis.Config();
 		System.out.println(conf);
-		final DexAnalysis analysis = new DexAnalysis(conf);
+		final BakSmaliAnalysis analysis = new BakSmaliAnalysis(conf);
 		final DexProgram prog = analysis.analyze("data/JLex.dex");
 		System.out.println(prog);
 	}
