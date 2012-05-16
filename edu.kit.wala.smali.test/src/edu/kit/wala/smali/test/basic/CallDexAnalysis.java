a08624d0f9e0d08f7af44cb4744883250be35d30
Juergen Graf <juergen.graf@gmail.com>
2012-05-16 22:51:51 +0200

    smali test: print out classes and methods and trigger graph (CFG,CDG,...) creation

--- edu.kit.wala.smali.test/src/edu/kit/wala/smali/test/basic/CallDexAnalysis.java
+++ edu.kit.wala.smali.test/src/edu/kit/wala/smali/test/basic/CallDexAnalysis.java
@@ -2,6 +2,8 @@ package edu.kit.wala.smali.test.basic;
 
 import org.jf.baksmali.Interface.BakSmaliAnalysis;
 import org.jf.dexlib.Interface.DexAnalysis.DexAnalysisException;
+import org.jf.dexlib.Interface.DexClass;
+import org.jf.dexlib.Interface.DexMethod;
 import org.jf.dexlib.Interface.DexProgram;
 
 public class CallDexAnalysis {
@@ -11,5 +13,35 @@ public class CallDexAnalysis {
 		final BakSmaliAnalysis analysis = new BakSmaliAnalysis(conf);
 		final DexProgram prog = analysis.analyze("data/JLex.dex");
 		System.out.println(prog);
+		
+		for (final DexClass cls : prog.getClasses()) {
+			System.out.println(cls);
+			
+			for (final DexMethod method : cls.getMethods()) {
+				System.out.print(method);
+				
+				System.out.print(" - computing graphs");
+				method.getControlFlowGraph(false);
+				System.out.print(".");
+				method.getControlFlowGraph(true);
+				System.out.print(".");
+				method.getDominanceFrontiers(false);
+				System.out.print(".");
+				method.getDominanceFrontiers(true);
+				System.out.print(".");
+				method.getDominators(false);
+				System.out.print(".");
+				method.getDominators(true);
+				System.out.print(".");
+				method.getDominationTree(false);
+				System.out.print(".");
+				method.getDominationTree(true);
+				System.out.print(".");
+				method.getControlDependenceGraph(false);
+				System.out.print(".");
+				method.getControlDependenceGraph(true);
+				System.out.println("done.");
+			}
+		}
 	}
 }
